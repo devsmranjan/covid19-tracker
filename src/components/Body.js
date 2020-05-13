c840d7f7d7d67042cb792bbb43eb985bf8ca6c61
smrutiranjanrana <smrutiranjan.developer@gmail.com>
2020-05-13 15:12:48 +0530

    add https api, add loader

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,7 +12,7 @@ const Body = () => {
     useEffect(() => {
         const checkVersionAPI = async () => {
             let response = await axios.get(
-                'http://devsmranjan.github.io/covid-19-tracker-api/v2/app_versions.json'
+                'https://devsmranjan.github.io/covid-19-tracker-api/v2/app_versions.json'
             );
 
             setLatestAppVersion(response.data['latest_app_version']);
@@ -26,44 +26,60 @@ const Body = () => {
 
     return (
         <div className='body absolute-center'>
-            <div>
-                <div className='logo-container'>
-                    <img
-                        className='logo'
-                        src={Logo}
-                        alt='COVID-19 Tracker logo'
-                    />
-                </div>
-                <h4 className='app-title'>COVID-19 Tracker</h4>
+            {latestAppVersion !== '' &&
+            downloadLink !== '' &&
+            alternate1Link !== '' &&
+            alternate1Link !== '' ? (
+                <div>
+                    <div className='logo-container'>
+                        <img
+                            className='logo'
+                            src={Logo}
+                            alt='COVID-19 Tracker logo'
+                        />
+                    </div>
+                    <h4 className='app-title'>COVID-19 Tracker</h4>
 
-                {latestAppVersion !== '' ? (
-                    <h4 className='version'>v{latestAppVersion}</h4>
-                ) : (
-                    <span></span>
-                )}
-                <div className='btn-group mt-24'>
-                    <a href={downloadLink} className='btn btn-style-1 btn-lg'>
-                        Download (64 bit)
-                    </a>
-                    <button
-                        type='button'
-                        className='btn btn-style-1 dropdown-toggle dropdown-toggle-split'
-                        data-toggle='dropdown'
-                        aria-haspopup='true'
-                        aria-expanded='false'>
-                        <span className='sr-only'>Download options</span>
-                    </button>
-                    <div className='dropdown-menu dropdown-menu-right'>
-                        <a className='dropdown-item' href={alternate1Link}>
-                            Alternative 1 (32 bit)
+                    {latestAppVersion !== '' ? (
+                        <h4 className='version'>v{latestAppVersion}</h4>
+                    ) : (
+                        <span></span>
+                    )}
+                    <div className='btn-group mt-24'>
+                        <a
+                            href={downloadLink}
+                            className='btn btn-style-1 btn-lg'>
+                            Download (64 bit)
                         </a>
-                        <div className='dropdown-divider'></div>
-                        <a className='dropdown-item' href={alternate2Link}>
-                            Alternative 2 (x86_64)
-                        </a>
+                        <button
+                            type='button'
+                            className='btn btn-style-1 dropdown-toggle dropdown-toggle-split'
+                            data-toggle='dropdown'
+                            aria-haspopup='true'
+                            aria-expanded='false'>
+                            <span className='sr-only'>Download options</span>
+                        </button>
+                        <div className='dropdown-menu dropdown-menu-right'>
+                            <a className='dropdown-item' href={alternate1Link}>
+                                Alternative 1 (32 bit)
+                            </a>
+                            <div className='dropdown-divider'></div>
+                            <a className='dropdown-item' href={alternate2Link}>
+                                Alternative 2 (x86_64)
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            ) : (
+                <div>
+                    <div
+                        className='spinner-border text-primary'
+                        style={{ width: '3rem', height: '3rem' }}
+                        role='status'>
+                        <span className='sr-only'>Loading...</span>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
